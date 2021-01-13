dee24cb04b31403548d31bfa9ccad43c6d9c1545
monia <moniatec@gmail.com>
2021-01-13 10:54:38 -0500

    adding routes to the app.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,24 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-     <h1> Here we go</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login</h1>
+          </Route>
+          <Route path='/'>
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
