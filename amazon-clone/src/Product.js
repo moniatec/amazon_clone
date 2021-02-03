62748f9832dea91bd32e2178c674a87d75d38715
monia <moniatec@gmail.com>
2021-02-03 15:52:50 -0500

    styling product card

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -3,7 +3,23 @@ import './Product.css'
 
 function Product({ id, title, images, price, rating }) {
     return (
-        <div>
+        <div className='product'>
+            <div className='product__info'>
+                <p>{title}</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='product__rating'>
+                    {Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>:star</p>
+                        ))}
+                </div>
+            </div>
+            <img src={image} alt='' />
+            <button>Add to basket</button>
 
         </div>
     )
