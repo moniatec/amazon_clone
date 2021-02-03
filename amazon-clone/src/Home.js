3c345047de8cb718fb5ca87e224f2db22e5ba764
monia <moniatec@gmail.com>
2021-02-03 14:28:39 -0500

    create Product compenent

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -7,6 +8,15 @@ function Home() {
             <img className='home__image'
                 src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Movie/SYLV_2020_GWBleedingHero_3000x1200_POST_Final_noLocale_PVD6240_SylviesLove._CB416257388_.jpgban"
                 alt=''
+            />
+
+
+            <Product
+                id='12321341'
+                title='The Lean Startup'
+                price={11.96}
+                rating={5}
+                image='https://s-i.huffpost.com/gen/1148926/images/o-CLASSIC-BOOKS-ORIGINAL-COVERS-facebook.jpg'
             />
         </div>
     )
---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css'
+
+function Product({ id, title, images, price, rating }) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Product
