e23fa37c1bb38a1d318c6ca6499eda4b73dcfd14
monia <moniatec@gmail.com>
2021-02-09 12:13:05 -0500

    create checkout component

--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -1,13 +1,16 @@
 export const initialState = {
     basket: [],
-    // user: null,
+    user: null,
 }
 
 function reducer(state, action) {
 
     switch (action.type) {
         case 'ADD_TO_BASKET':
-            return { state }
+            return {
+                ...state,
+                basket: [...state.basket, action.item]
+            }
             break;
         case 'REMOVE_FROM_BASKET':
             return { state }
